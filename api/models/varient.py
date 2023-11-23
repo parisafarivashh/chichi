3f0080650a84010e864fd144691c4200fda603d2
parisa <parisafarivash@gmail.com>
2023-11-23 19:37:37 +0330

    Change varient model

--- api/models/varient.py
+++ api/models/varient.py
@@ -6,22 +6,14 @@ from .product import Product
 
 class Variant(models.Model):
 
-    class SizeTypeChoice(models.TextChoices):
-        L = 'l'
-        M = 'm'
-        S = 's'
-        xl = 'xl'
-        xxl = 'xxl'
+    class StatusChoice(models.TextChoices):
+        success = 'success'
+        fail = 'fail'
 
-    product = models.ForeignKey(
-        Product, on_delete=models.CASCADE, related_name='varients'
-    )
-    color = models.ForeignKey(
-        Color, on_delete=models.CASCADE, related_name='varients'
-    )
-    size = models.CharField(
-        max_length=16, choices=SizeTypeChoice.choices, null=True, blank=True
-    )
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='varients')
+    color = models.ForeignKey(Color, on_delete=models.CASCADE, related_name='varients')
+    size = models.CharField(max_length=30,  null=True, blank=True)
+    status = models.CharField(max_length=30, choices=StatusChoice.choices, null=True, blank=True)
     price = models.DecimalField(decimal_places=2, max_digits=10, default=0)
     count = models.IntegerField(default=0, null=False, blank=False)
 
